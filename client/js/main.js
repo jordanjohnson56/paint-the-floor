baffa34b606b0b3be847e883f6734c10dfa1ed3c
Jordan Johnson <jajohnson46@students.tntech.edu>
2017-06-11 05:03:36 +0000

    Initialize repository

--- client/js/main.js
+++ client/js/main.js
@@ -1,59 +1,125 @@
-// var cv = document.getElementById('cvs');
-// var ct = cv.getc('2d');
-
 /* global io */
 /* global $ */
 $(function() {
-  var socket = io();
+  var socket = io();      // Client socket.
   
-  var has_joined = false;
-  var canvas, c;
-  var grid_w = 75;
-  var grid_h = 40;
-  var grid = [];
-  var players = [];
-  var can_render = false;
+  var has_joined = false; // Has client joined a room?
+  var canvas, c;          // Game canvas and context.
+  var grid_w = 75;        // Number of horizontal grid tiles.
+  var grid_h = 40;        // Number of vertical grid tiles.
+  var view_w = 30;        // Number of horizontal grid tiles shown to player.
+  var view_h = 16;        // Number of vertical grid tiles shown to player.
+  var grid = [];          // Current state of the game board.
+  var players = [];       // Current player stats.
+  var can_render = false; // Has client received first snapshot?
+  var my_id, my_player;   // Client's player id and associated object.
 
+  // Resize the canvas when the window is resized.
   window.addEventListener('resize', resize);
   
-  // $('form').submit(function() {
+  // Join room when player submits, if valid info is given.
+  $('#join-room').submit(function(e) {
+    e.preventDefault();
+    var name_input = $('#player-name').val();
+    var room_input = $('#room-code').val();
     if(!has_joined) {
-      // Tell the server
-      // socket.emit('message', {
-      //   player_name: $('#player-name').val(),
-      //   room_code: $('#room-code').val()
-      // });
-      
-      // Remove the join screen
-      $('#join').remove();
-      has_joined = true;
-      
-      // Create the canvas
-      createCanvas();
-      // Start the rendering loop
-      window.requestAnimationFrame(draw);
+      if(name_input != '') {
+        if(room_input != '') {
+          // Tell the server
+          socket.emit('join_room', {
+            player_name: name_input,
+            room_code: room_input
+          });
+        } else {
+          console.error('Please enter a room code.');
+        }
+      } else {
+        console.error('Please enter a name.');
+      }
     }
-    // return false;
-  // });
-  
-  socket.on('update', function(data) {
-    console.log('Received update!');
-    for(var i = 0; i < data.length; i++) {
-      grid.push(data[i]);
-    }
+    return false;
   });
   
+  // Create room when player submits, if valid info is given.
+  $('#create-room').submit(function(e) {
+    e.preventDefault();
+    var name_input = $('#player-name').val();
+    var room_input = $('#room-code').val();
+    if(!has_joined) {
+      if(name_input != '') {
+        if(room_input != '') {
+          // Tell the server
+          socket.emit('create_room', {
+            player_name: name_input,
+            room_code: room_input
+          });
+        } else {
+          showError('Please enter a room code.');
+        }
+      } else {
+        showError('Please enter a name.');
+      }
+    }
+    return false;
+  });
+  
+  // Error returned when a room cannot be created.
+  socket.on('could_not_create_room', function() {
+    showError('Could not create room.');
+  });
+  
+  // Error returned when a room cannot be joined.
+  socket.on('could_not_join_room', function() {
+    showError('Could not join room.');
+  });
+  
+  // Notification that a room was successfully joined.
+  socket.on('joined_room', function(player) {
+    console.log('hello room');
+    // Remove the join screen
+    $('#join').remove();
+    has_joined = true;
+    
+    my_id = player.id;
+
+    // Create the canvas
+    createCanvas();
+    // Start the rendering loop
+    window.requestAnimationFrame(draw);
+  });
+  
+  // Latest game state from the server.
+  // Includes grid state, player states (positions), as well as some metrics to
+  // help prevent cheating.
   socket.on('snapshot', function(state) {
-    grid = state.grid_state;
-    players = state.players;
+    getStateInfo(state);
   });
   
+  // Initial game state from server which tells the client that it can start
+  // rendering the game.
   socket.on('snapshot_start', function(state) {
-    grid = state.grid_state;
-    players = state.players;
+    getStateInfo(state);
     can_render = true;
   });
   
+  // Updates client's game state information based on server state.
+  function getStateInfo(state) {
+    grid = state.grid_state;
+    players = state.players;
+    
+    // The best anti-cheat I can come up with.
+    grid_w = state.grid_w;
+    grid_h = state.grid_h;
+    view_w = state.view_w;
+    view_h = state.view_h;
+    
+    players.forEach(function(player) {
+      if(player.id == my_id) my_player = player;
+    });
+  }
+  
+  // Watch for arrow key presses and send this information to the server so that
+  // it can move the player in the correct direction.
   document.addEventListener("keydown", function(event){
     switch(event.code){
       case 'ArrowLeft': 
@@ -71,6 +137,7 @@ $(function() {
     }
   });
   
+  // Create the game canvas.
   function createCanvas() {
     $('body').append('<canvas id="game_canvas"></canvas>');
     canvas = $('#game_canvas')[0];
@@ -79,23 +146,27 @@ $(function() {
     resize();
   }
   
+  // Main game rendering loop. This is the client's primary job.
   function draw() {
     if(c !== undefined) {
       // Clear canvas
       c.clearRect(0, 0, canvas.width, canvas.height);
-
+      // Draw gridlines.
       drawGrid();
-      
+      // Fill grid with game state info.
       fillGrid();
+      // Draw minimap.
+      drawMap();
     }
     window.requestAnimationFrame(draw);
   }
   
+  // Draw the grid lines on the canvas based on screen height and width.
   function drawGrid() {
     var c_width = canvas.width;
     var c_height = canvas.height;
     
-    var gap = c_width / grid_w;
+    var gap = c_width / view_w;
     c.lineCap = 'square';
     c.strokeStyle = '#D3D3D3';
     c.lineWidth = 1;
@@ -108,7 +179,7 @@ $(function() {
       c.stroke();
     }
     
-    gap = c_height / grid_h;
+    gap = c_height / view_h;
     
     // Draw horizontal lines
     for(var i = 0; i < c_height; i += gap) {
@@ -119,17 +190,23 @@ $(function() {
     }
   }
   
+  // Fill the grid with colored squares and other player's positions.
   function fillGrid() {
     var c_width = canvas.width;
     var c_height = canvas.height;
     
-    var gap_w = c_width / grid_w;
-    var gap_h = c_height / grid_h;
+    var gap_w = c_width / view_w;
+    var gap_h = c_height / view_h;
+    
+    var view_x;
+    var view_y;
     
     if(can_render) {
-      for(var i = 0; i < grid_w; i++) {
-        for(var j = 0; j < grid_h; j++) {
-          c.fillStyle = grid[i][j];
+      view_x = clamp(my_player.x - Math.floor(view_w / 2), 0, grid_w - view_w);
+      view_y = clamp(my_player.y - Math.floor(view_h / 2) + 1, 0, grid_h - view_h);
+      for(var i = 0; i < view_w; i++) {
+        for(var j = 0; j < view_h; j++) {
+          c.fillStyle = grid[i + view_x][j + view_y];
           var x = i * gap_w + 1;
           var y = j * gap_h + 1;
           c.fillRect(x, y, gap_w - 2, gap_h - 2);
@@ -137,20 +214,71 @@ $(function() {
       }
       
       c.strokeStyle = '#000';
-      c.lineWidth = 2;
+      c.lineWidth = 8;
       players.forEach(function(player) {
-        var x = player.x * gap_w + 1;
-        var y = player.y * gap_h + 1;
-        c.strokeRect(x, y, gap_w - 2, gap_h - 2);
+        var rel_x = player.x - view_x;
+        var rel_y = player.y - view_y;
+        var x = rel_x * gap_w + Math.ceil(c.lineWidth / 2) + 1;
+        var y = rel_y * gap_h + Math.ceil(c.lineWidth / 2) + 1;
+        c.strokeRect(x, y, gap_w - 2 * (Math.ceil(c.lineWidth / 2) + 1),
+                           gap_h - 2 * (Math.ceil(c.lineWidth / 2) + 1));
       });
     }
   }
   
+  // Draw the minimap in the top-left corner with the full game state
+  // information.
+  function drawMap() {
+    var map_x = 5;
+    var map_y = 5;
+    var map_border = 2;
+    var map_w = Math.floor(canvas.width / 7);
+    var map_h = Math.floor(canvas.height / 7);
+    
+    var gap_w = (map_w - map_border * 2) / grid_w;
+    var gap_h = (map_h - map_border * 2) / grid_h;
+    
+    if(can_render) {
+      c.fillStyle = '#000';
+      c.fillRect(map_x, map_y, map_w, map_h);
+      
+      for(var i = 0; i < grid_w; i++) {
+        for(var j = 0; j < grid_h; j++) {
+          c.fillStyle = grid[i][j];
+          var x = map_x + map_border + i * gap_w;
+          var y = map_y + map_border + j * gap_h;
+          c.fillRect(x, y, gap_w, gap_h);
+        }
+      }
+      
+      c.strokeStyle = '#000';
+      c.lineWidth = 1;
+      players.forEach(function(player) {
+        var x = map_x + map_border + player.x * gap_w;
+        var y = map_y + map_border + player.y * gap_h;
+        c.strokeRect(x, y, gap_w, gap_h);
+      });
+    }
+  }
+  
+  // Resize the game canvas to fill the window.
   function resize() {
     if(c !== undefined) {
       c.canvas.width = window.innerWidth;
       c.canvas.height = window.innerHeight;
     }
+  }
+  
+  // Show an error message.
+  function showError(error) {
+    console.error(error);
+  }
+  
+  // Utility to contain a value within a minimum and maximum.
+  function clamp(value, min, max) {
+    if(value < min) return min;
+    if(value > max) return max;
+    return value;
   }
 
 });
--- server.js
+++ server.js
@@ -1,89 +1,347 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var path = require('path');
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
-
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-    
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        if(err) return console.log(err);
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        if(err) return console.log(err);
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      if(err) return console.log(err);
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-console.log(process.argv);
-
-server.listen(process.argv[3] || process.env.PORT || 8080, process.argv[2] || process.env.IP || "http://troy-mayjor.c9users.io", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+// Default server setup.
+http.listen(Number(process.argv[3]), process.argv[2], function() {
+  console.log('listening on ' + process.argv[2] + ':' + process.argv[3]);
 });
+
+// Look for resources in these folders.
+app.use('/css', express.static(path.join('client', 'css')));
+app.use('/js', express.static(path.join('client', 'js')));
+app.use('/img', express.static(path.join('client', 'img')));
+
+// Send splash.html to new clients.
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+// Player colors and base tile color.
+// Note: need to add gray as a color, but this requires some changes to other
+// bits of code.
+const COLORS = [
+  '#FFFFFF', // white
+  '#ED1C24', // red
+  '#333399', // blue
+  '#00A550', // green
+  '#FFEF00'  // yellow
+];
+const GRID_WIDTH = 75;  // Number of horizontal grid tiles.
+const GRID_HEIGHT = 40; // Number of vertical grid tiles.
+const MAX_PLAYERS = 4;  // Number of players allowed in room.
+const GAME_TICK = 100;  // Milliseconds between server ticks.
+const VIEW_WIDTH = 30;  // Number of horizontal grid tiles shown to player.
+const VIEW_HEIGHT = 16; // Number of vertical grid tiles shown to player.
+
+var rooms = {};         // List of currently created rooms.
+var intervals = {};     // Parallel array for intervals created to update the rooms.
+
+class GameRoom {
+  // Sets room name, grid width and height, sets all grid tiles to white,
+  // creates the four "ai" players, and draw any obstacles.
+  constructor(name) {
+    this.name = name;
+    this.grid_w = GRID_WIDTH;
+    this.grid_h = GRID_HEIGHT;
+    this.grid_state = [];
+  
+    for(var i = 0; i < this.grid_w; i++) {
+      this.grid_state[i] = [];
+      for(var j = 0; j < this.grid_h; j++) {
+        this.grid_state[i][j] = COLORS[0];
+      }
+    }
+    
+    // AI
+    // Note: will be removed once lobbies are set up.
+    this.player1 = {
+      id: '1',
+      x: Math.floor(Math.random() * this.grid_w),
+      y: Math.floor(Math.random() * this.grid_h),
+      color: COLORS[1],
+      dir: Math.floor(Math.random() * 4)
+    };
+    
+    this.player2 = {
+      id: '2',
+      x: Math.floor(Math.random() * this.grid_w),
+      y: Math.floor(Math.random() * this.grid_h),
+      color: COLORS[2],
+      dir: Math.floor(Math.random() * 4)
+    };
+    
+    this.player3 = {
+      id: '3',
+      x: Math.floor(Math.random() * this.grid_w),
+      y: Math.floor(Math.random() * this.grid_h),
+      color: COLORS[3],
+      dir: Math.floor(Math.random() * 4)
+    };
+    
+    this.player4 = {
+      id: '4',
+      x: Math.floor(Math.random() * this.grid_w),
+      y: Math.floor(Math.random() * this.grid_h),
+      color: COLORS[4],
+      dir: Math.floor(Math.random() * 4)
+    };
+    
+    // Array of all players in the room.
+    this.players = [
+      this.player1,
+      this.player2,
+      this.player3,
+      this.player4
+    ];
+    
+    // Draw obstacle in the center.
+    this.center_w = Math.floor(this.grid_w / 2);
+    this.center_h = Math.floor(this.grid_h / 2);
+    this.drawRectObstacle(this.center_w - 8, this.center_h - 4, 16, 8);
+    
+    // Confirm that the AI don't spawn in a wall.
+    this.verifyPosition(this.player1);
+    this.verifyPosition(this.player2);
+    this.verifyPosition(this.player3);
+    this.verifyPosition(this.player4);
+  }
+  
+  // Main game loop for this room. Gets put into a setInterval function.
+  updateGame() {
+    // Give AI random move directions.
+    this.player1.dir = Math.floor(Math.random() * 4);
+    this.player2.dir = Math.floor(Math.random() * 4);
+    this.player3.dir = Math.floor(Math.random() * 4);
+    this.player4.dir = Math.floor(Math.random() * 4);
+    
+    // Update each of the player positions based on direction.
+    this.players.forEach(function(player) {
+      this.updatePosition(player);
+      this.grid_state[player.x][player.y] = player.color;
+    }, this);
+    
+    // Tell each of the clients in the room.
+    io.to(this.name).emit('snapshot', this.roomState);
+  }
+  
+  // Updates player's position based on direction, but will ensure player does
+  // not go where he is not supposed to go.
+  updatePosition(player) {
+    if(player.dir == 0) {
+      // UP
+      if(this.playerCanBe(player.x, player.y - 1, player.color)) player.y -= 1;
+    } else if(player.dir == 1) {
+      // RIGHT
+      if(this.playerCanBe(player.x + 1, player.y, player.color)) player.x += 1;
+    } else if(player.dir == 2) {
+      // DOWN
+      if(this.playerCanBe(player.x, player.y + 1, player.color)) player.y += 1;
+    } else {
+      // LEFT
+      if(this.playerCanBe(player.x - 1, player.y, player.color)) player.x -= 1;
+    }
+  }
+  
+  // Checks a position (x,y) and returns true if a player (color) can go to it.
+  playerCanBe(x, y, color) {
+    if(!this.posInBounds(x,y)) return false;
+    if(this.posColor(x,y) != COLORS[0] && this.posColor(x,y) != color) {
+      return false;
+    }
+    return true;
+  }
+  
+  // Checks if a position (x,y) is inside the bounds of the grid.
+  posInBounds(x, y) {
+    if(x < 0 || x >= this.grid_w) return false;
+    if(y < 0 || y >= this.grid_h) return false;
+    return true;
+  }
+  
+  // Returns the color of a grid square.
+  posColor(x, y) {
+    return this.grid_state[x][y];
+  }
+  
+  // Draws a rectangular obstacle on the board.
+  drawRectObstacle(x, y, w, h) {
+    for(var i = x; i < x + w; i++) {
+      for(var j = y; j < y + h; j++) {
+        this.grid_state[i][j] = '#686868';
+      }
+    }
+  }
+  
+  // Adds a player to the GameRoom.
+  addPlayer(player) {
+    this.verifyPosition(player);
+    this.players.push(player);
+  }
+  
+  // Removes a player from the GameRoom (if he exists).
+  removePlayer(player) {
+    var index = this.players.indexOf(player);
+    if(index >= 0) {
+      this.players.splice(index, 1);
+    }
+    if(this.players.length <= 4) {
+      destroyRoom(this.name);
+    }
+  }
+  
+  // Make sure player does not spawn in a wall.
+  verifyPosition(player) {
+    while(!this.playerCanBe(player.x, player.y, player.color)) {
+      player.x = Math.floor(Math.random() * GRID_WIDTH);
+      player.y = Math.floor(Math.random() * GRID_HEIGHT);
+    }
+  }
+  
+  // Number of players in the room.
+  get size() {
+    return this.players.length - 4; // Number of players that are not "ai".
+  }
+  
+  // State of the room.
+  get roomState() {
+    var state = {};
+    state.grid_state = this.grid_state;
+    state.players = this.players;
+    state.grid_w = GRID_WIDTH;
+    state.grid_h = GRID_HEIGHT;
+    state.view_w = VIEW_WIDTH;
+    state.view_h = VIEW_HEIGHT;
+    return state;
+  }
+}
+
+// Things related to a new connection.
+io.sockets.on('connection', function(client) {
+  console.log('Client connection received. IP: '
+              + client.handshake.address);
+  
+  var room;   // Player's room name.
+  var player; // Player object.
+  
+  // Remove player from room when he disconnects.
+  client.on('disconnect', function() {
+    console.log('Client disconnected. IP: '
+                + client.request.connection.remoteAddress);
+    if(room !== undefined) {
+      leaveRoom(room, player);
+    }
+  });
+  
+  // Request to join a room.
+  client.on('join_room', function(data) {
+    var player_name = data.player_name.toString();
+    var room_code = data.room_code.toString();
+    
+    console.log('Player ' + player_name + ' is connecting to room '
+                + room_code + '.');
+    
+    // Connect player to room if possible, otherwise send error.
+    ifJoinRoom(room_code);
+  });
+  
+  // Create a new room if possible.
+  client.on('create_room', function(data) {
+    var player_name = data.player_name.toString();
+    var room_code = data.room_code.toString();
+    
+    console.log('Creating room. Name: ' + room_code);
+    
+    if(createRoom(room_code)) {
+      ifJoinRoom(room_code);
+    } else {
+      client.emit('could_not_create_room');
+    }
+  });
+  
+  // Update player's direction.
+  client.on('direction', function(dir) {
+    if(player !== undefined) {
+      player.dir = dir;
+    }
+  });
+  
+  // Join a room if possible.
+  function ifJoinRoom(room_name) {
+    if(canJoinRoom(room_name)) {
+      room = room_name;
+      player = createPlayer(client.id);
+      client.join(room);
+      // Add player to room
+      joinRoom(room, player);
+      // Notify client
+      client.emit('joined_room', player);
+      // Send initial snapshot of game board
+      client.emit('snapshot_start', getRoomState(room));
+    } else {
+      client.emit('could_not_join_room');
+    }
+  }
+});
+
+// Create a player object with a given id.
+function createPlayer(id) {
+  var player = {
+    id: id,
+    x: Math.floor(Math.random() * GRID_WIDTH),
+    y: Math.floor(Math.random() * GRID_HEIGHT),
+    color: COLORS[Math.floor((Math.random() * (COLORS.length - 1)) + 1)],
+    dir: Math.floor(Math.random() * 4)
+  };
+  return player;
+}
+
+// Checks if the room exists and has an open spot.
+function canJoinRoom(room_name) {
+  return (room_name in rooms) && (rooms[room_name].size < MAX_PLAYERS);
+}
+
+// Create a new room.
+function createRoom(room_name) {
+  if(!(room_name in rooms)) {
+    rooms[room_name] = new GameRoom(room_name);
+    intervals[room_name] = 
+      setInterval(rooms[room_name].updateGame.bind(rooms[room_name]), 
+                  GAME_TICK);
+    return true;
+  }
+  return false;
+}
+
+// Destroy a room.
+function destroyRoom(room_name) {
+  if(room_name in rooms) {
+    delete rooms[room_name];
+    clearInterval(intervals[room_name]);
+    delete intervals[room_name];
+  }
+}
+
+// Add a player to a specific room.
+function joinRoom(room_name, player) {
+  if(canJoinRoom(room_name)) {
+    rooms[room_name].addPlayer(player);
+  }
+}
+
+// Remove a player from a specific room.
+function leaveRoom(room_name, player) {
+  if(room_name in rooms) {
+    rooms[room_name].removePlayer(player);
+  }
+}
+
+// Get room state for new players
+function getRoomState(room_name) {
+  if(room_name in rooms) {
+    return rooms[room_name].roomState;
+  }
+  return {};
+}
